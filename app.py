459770e9da98ff97466d58d5af938cb77b491314
Himanshu <hs127010@gmail.com>
2022-12-22 00:04:16 +0530

    complete

--- app.py
+++ app.py
@@ -8,27 +8,46 @@ app=Flask(__name__)
 def home():
     if request.method == 'POST':
         user_input = request.form.get('user_input')
-        print(user_input)
-
-        #user_input = 'delhi'
-        complete_api_link1 ="http://api.openweathermap.org/geo/1.0/direct?q="+user_input+"&limit=5&appid=52e40abca47fa344472973b265b5c319"
+        k=str(user_input)
+        complete_api_link1 ="http://api.openweathermap.org/geo/1.0/direct?q="+k+"&limit=5&appid=52e40abca47fa344472973b265b5c319"
         location = requests.get(complete_api_link1)
-        #print(location.status_code)
+            #print(location.status_code)
         loc = location.json()
         #print(location.text)
-        lat = str(loc[2]['lat'])
-        lon = str(loc[2]['lon'])
+        a=float(loc[2]['lat'])
+        b=float(loc[2]['lon'])
+        lat = str(a)
+        lon = str(b)
         complete_api_link ="https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&appid=52e40abca47fa344472973b265b5c319"
         a = requests.get(complete_api_link)
         data = a.json()
-        #print(a.status_code)
-        #print(data)
-        temp = data['main']['temp']
+    #print(a.status_code)
+    #print(data)
+        temp1 = '%.2f' % (abs(data['main']['temp']-273))
+        temp2 = '%.4f' % (abs(data['main']['temp_min']-273))
+        temp3 = '%.4f' % (abs(data['main']['temp_max']-273))
+        temp4 = data['main']['humidity']
+        temp5 = data['main']['pressure']
+        temp6 = data['wind']['speed']
+        temp7 = data['wind']['deg']
+        temp8 = data['weather'][0]['description']
+        temp9 = data['visibility']
+        temp10 = data['weather'][0]['main']
+        result=[]
+        result.append(temp1)
+        result.append(temp2)
+        result.append(temp3)
+        result.append(temp4)
+        result.append(temp5)
+        result.append(temp6)
+        result.append(temp7)
+        result.append(temp8)
+        result.append(temp9)
+        result.append(temp10)
+        return render_template('search.html',data=result)
 
-
-        return render_template("index.html", temp=temp)
     else:
-        return render_template('index.html')
+       return render_template('index.html')
 
     
 
@@ -37,4 +56,4 @@ def home():
 
 
 if __name__ =="__main__":
-    app.run(debug=False, port=8000)
+    app.run(debug=True, port=8000)
